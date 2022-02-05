e0fde6dc3c6ca60ece1a81cb04d444da762ce733
cvsmith <chris@chrissmith.tech>
2022-02-05 18:19:24 -0500

    add goals homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "goals#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/goals", to: "goals#index"
+
 end
