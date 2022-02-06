bf7ff172079fd22a5cd1c25271bda2d53d67e41b
cvsmith <chris@chrissmith.tech>
2022-02-05 19:12:00 -0500

    add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_001009) do
+ActiveRecord::Schema.define(version: 2022_02_06_001140) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
 
   create_table "goals", force: :cascade do |t|
     t.string "name"
@@ -30,5 +38,6 @@ ActiveRecord::Schema.define(version: 2022_02_06_001009) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "goals", "categories"
 end
